463e96bd9d37dad0855cc681903691bb07e2fe3b
THIAGO MARTIRO DE OLIVEIRA <thiago.moliveira31@senacsp.edu.br>
2025-02-28 22:12:38 -0300

    Login3

--- Login/Form1.Designer.cs
+++ Login/Form1.Designer.cs
@@ -34,21 +34,28 @@
             Usuário = new Label();
             Senha = new Label();
             LbResultado = new Label();
+            New = new Button();
+            Newus = new TextBox();
+            Novasenha = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            resultado = new Label();
             SuspendLayout();
             // 
             // Entrar
             // 
-            Entrar.Location = new Point(380, 250);
+            Entrar.Location = new Point(383, 241);
             Entrar.Name = "Entrar";
             Entrar.Size = new Size(75, 23);
             Entrar.TabIndex = 0;
-            Entrar.Text = "Entrar";
+            Entrar.Text = "Cadastro";
             Entrar.UseVisualStyleBackColor = true;
             Entrar.Click += Entrar_Click;
             // 
             // TxtSenha
             // 
-            TxtSenha.Location = new Point(337, 221);
+            TxtSenha.Location = new Point(337, 214);
             TxtSenha.Name = "TxtSenha";
             TxtSenha.Size = new Size(159, 23);
             TxtSenha.TabIndex = 1;
@@ -86,11 +93,77 @@
             LbResultado.Size = new Size(0, 15);
             LbResultado.TabIndex = 5;
             // 
+            // New
+            // 
+            New.Location = new Point(817, 238);
+            New.Name = "New";
+            New.Size = new Size(75, 26);
+            New.TabIndex = 6;
+            New.Text = "New";
+            New.UseVisualStyleBackColor = true;
+            New.Click += New_Click;
+            // 
+            // Newus
+            // 
+            Newus.Location = new Point(768, 180);
+            Newus.Name = "Newus";
+            Newus.Size = new Size(162, 23);
+            Newus.TabIndex = 7;
+            // 
+            // Novasenha
+            // 
+            Novasenha.Location = new Point(768, 209);
+            Novasenha.Name = "Novasenha";
+            Novasenha.Size = new Size(162, 23);
+            Novasenha.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(703, 217);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Senha";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(703, 183);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Usuário";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(813, 150);
+            label3.Name = "label3";
+            label3.Size = new Size(79, 15);
+            label3.TabIndex = 11;
+            label3.Text = "Novo Usuário";
+            // 
+            // resultado
+            // 
+            resultado.AutoSize = true;
+            resultado.Location = new Point(828, 315);
+            resultado.Name = "resultado";
+            resultado.Size = new Size(0, 15);
+            resultado.TabIndex = 12;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1082, 450);
+            Controls.Add(resultado);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Novasenha);
+            Controls.Add(Newus);
+            Controls.Add(New);
             Controls.Add(LbResultado);
             Controls.Add(Senha);
             Controls.Add(Usuário);
@@ -111,5 +184,12 @@
         private Label Usuário;
         private Label Senha;
         private Label LbResultado;
+        private Button New;
+        private TextBox Newus;
+        private TextBox Novasenha;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label resultado;
     }
 }
--- Login/Form1.cs
+++ Login/Form1.cs
@@ -1,3 +1,4 @@
+using System.DirectoryServices.ActiveDirectory;
 using System.Drawing;
 
 namespace Login
@@ -5,8 +6,8 @@ namespace Login
     public partial class Form1 : Form
     {
 
-        List<string> listaUsuario = new List<string>() { "neymar.jr", "pablo.vitar","naruto"};
-        List<string> listaSenha = new List<string>() { "Bruna", "1234","7777" };
+        List<string> listaUsuario = new List<string>() { "neymar.jr", "pablo.vitar", "naruto" };
+        List<string> listaSenha = new List<string>() { "Bruna", "1234", "7777" };
         public Form1()
         {
             InitializeComponent();
@@ -14,11 +15,11 @@ namespace Login
 
         private void Entrar_Click(object sender, EventArgs e)
         {
-           
+
             String usuarioBuscado = TxtUsuario.Text; //Estamos_Pegando_Usuario_Do_Forms
             String senha = TxtSenha.Text; //Estamos_Pegando_Senha_Do_Forms
 
-            if  (string.IsNullOrWhiteSpace(usuarioBuscado))//Se_o_Usuario_Estiver_Nulo_Ou_Vazio//Utilizamos_IF_Para_CondiçãoVerdadeiraOuFalsa
+            if (string.IsNullOrWhiteSpace(usuarioBuscado))//Se_o_Usuario_Estiver_Nulo_Ou_Vazio//Utilizamos_IF_Para_CondiçãoVerdadeiraOuFalsa
             {
                 LbResultado.Text = "Preencha o usuário";//Usamos_Para_Mostrar_O_Resultado
                 LbResultado.ForeColor = Color.Red;
@@ -37,7 +38,7 @@ namespace Login
                 if (usuarioBuscado == listaUsuario[i])
                 {
                     posicaoUsuarioEncontrado = i;
-                  
+
                 }
             }
             if (posicaoUsuarioEncontrado == -1 || senha != "1234")
@@ -52,7 +53,36 @@ namespace Login
                 LbResultado.ForeColor = Color.Green;
             }
 
-            
+
         }
+
+       
+
+        private void New_Click(object sender, EventArgs e)
+        {
+            String NovoUs = Newus.Text;
+            String NovaSe = Novasenha.Text;
+
+            Boolean Usuario_encontrado=false;
+            for (int i = 0;i<listaUsuario.Count;i++)
+            {
+                if (NovoUs==listaUsuario[i])
+                {
+                    Usuario_encontrado= true;
+                }
+            }
+            if (Usuario_encontrado == false)
+            {
+              listaUsuario.Add(NovoUs);
+              listaSenha.Add(NovaSe);
+              resultado.Text = "Cadastrado com sucesso";
+            }
+            else
+            {
+                resultado.Text = "Ja exite";
+            }
+
+        }
+
     }
 }
