dfdd53d251904666e414202e08e113e0f1c1b911
THIAGO MARTIRO DE OLIVEIRA <thiago.moliveira31@senacsp.edu.br>
2025-04-11 22:20:31 -0300

    atividade

---
+++ listaatividades/banco-de-dados/table_atividades.sql
@@ -0,0 +1,16 @@
+CREATE TABLE atividade (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(100) NOT NULL,
+    situação INT NOT NULL DEFAULT 0
+);
+
+insert into atividade (titulo) values (@titulo);
+
+UPDATE atividade 
+
+SET situacao = @situacao WHERE id = @id;
+
+SELECT * FROM atividade WHERE situacao = 1;
+
+SELECT * FROM atividade WHERE situacao = 0;
+
